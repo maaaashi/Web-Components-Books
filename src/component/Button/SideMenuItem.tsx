a0c94c1a8baebb643aa5bd5ea17be29d24044f2a
maaaashi <doyaseizin.7@gmail.com>
2024-03-19 07:49:44 +0900

    side menu item compoennt

---
+++ src/component/Button/SideMenuItem.tsx
@@ -0,0 +1,17 @@
+import React, { type FC } from 'react'
+
+type Props = {
+  id: string
+  title: string
+}
+
+export const SideMenuItem: FC<Props> = ({ title, id }) => {
+  const move = () => {
+    window.location.href = `/webcomponents/${id}`  
+  }
+  return (
+    <button className="btn w-full justify-start" onClick={move}>
+      {title}
+    </button>
+  )
+}
