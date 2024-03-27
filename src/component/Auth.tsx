7c3727851eb0d58b15fe6c28de211af1466cfda4
maaaashi <doyaseizin.7@gmail.com>
2024-03-28 08:00:22 +0900

    signup signin button

--- src/component/Auth.tsx
+++ src/component/Auth.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { supabase } from '../libs/supabaseClient'
 import type { Session } from '@supabase/supabase-js'
 
-export const AccountInfo = () => {
+export const Auth = () => {
   const [session, setSession] = useState<null | Session>(null)
   useEffect(() => {
     supabase.auth.getSession().then((session) => {
@@ -10,9 +10,21 @@ export const AccountInfo = () => {
     })
   })
 
+  const signUp = async () => {}
+  const signIn = async () => {}
+
   if (session) {
     return <div>ログイン済</div>
   } else {
-    return <div>未ログイン</div>
+    return (
+      <div className='flex gap-2'>
+        <button className='btn btn-outline' onClick={signUp}>
+          Sign Up
+        </button>
+        <button className='btn' onClick={signIn}>
+          Sign In
+        </button>
+      </div>
+    )
   }
 }
