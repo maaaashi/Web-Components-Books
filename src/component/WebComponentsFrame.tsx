0e9f6e6a74f98bd4f3c2cb738cde4120c012769c
maaaashi <doyaseizin.7@gmail.com>
2024-03-27 23:36:38 +0900

    dark mode preveiw

--- src/component/WebComponentsFrame.tsx
+++ src/component/WebComponentsFrame.tsx
@@ -13,7 +13,7 @@ export const WebComponentsFrame: FC<Props> = () => {
       'country-flag',
       [
         new Attr('code', 'country code', 'JP', new Control('US')),
-        new Attr('size', '16 | 24 | 32 | 48 | 64', 32, new Control(32)),
+        new Attr('size', '16 | 24 | 32 | 48 | 64', 64, new Control(32)),
         new Attr(
           'type',
           'flat | shiny',
@@ -24,6 +24,8 @@ export const WebComponentsFrame: FC<Props> = () => {
       'https://maaaashi.github.io/country-flag/bundle.js',
     ),
   )
+  const [value, setValue] = useState<'light' | 'dark'>('light')
+
   const iframeRef = useRef<HTMLIFrameElement>(null)
 
   useEffect(() => {
@@ -50,12 +52,53 @@ export const WebComponentsFrame: FC<Props> = () => {
   return (
     <>
       <h2 className='font-bold text-lg'>{component.name}</h2>
-      <iframe
-        ref={iframeRef}
-        sandbox='allow-scripts'
-        style={{ width: '100%', height: '500px' }}
-        title='Sandboxed Component'
-      ></iframe>
+      <div className='relative'>
+        <div className='w-40 absolute right-0 top-3'>
+          <label className='cursor-pointer grid place-items-center'>
+            <input
+              type='checkbox'
+              value={value}
+              onChange={(e) => setValue(e.target.checked ? 'dark' : 'light')}
+              className='toggle bg-base-content row-start-1 col-start-1 col-span-2'
+            />
+            <svg
+              className={`col-start-1 row-start-1 ${value === 'light' ? 'stroke-stone-900 fill-stonestroke-stone-900' : 'stroke-stone-200 fill-stonestroke-stone-200'}`}
+              xmlns='http://www.w3.org/2000/svg'
+              width='14'
+              height='14'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <circle cx='12' cy='12' r='5' />
+              <path d='M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4' />
+            </svg>
+            <svg
+              className={`col-start-2 row-start-1 ${value === 'light' ? 'stroke-stone-900 fill-stonestroke-stone-900' : 'stroke-stone-200 fill-stonestroke-stone-200'}`}
+              xmlns='http://www.w3.org/2000/svg'
+              width='14'
+              height='14'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z'></path>
+            </svg>
+          </label>
+        </div>
+        <iframe
+          ref={iframeRef}
+          sandbox='allow-scripts'
+          className={`w-full h-96 ${value === 'dark' ? 'bg-stone-700' : ''}`}
+          title='Sandboxed Component'
+        ></iframe>
+      </div>
       <h3 className='font-bold text-lg'>Usage</h3>
       <pre className='bg-base-200 p-4'>{component.createHTMLElement()}</pre>
       <table className='table'>
