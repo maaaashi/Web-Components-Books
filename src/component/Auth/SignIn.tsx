2d3225545857384e4022a2175f8f0ab1cb2d68f0
maaaashi <doyaseizin.7@gmail.com>
2024-03-30 00:39:57 +0900

    add github login

--- src/component/Auth/SignIn.tsx
+++ src/component/Auth/SignIn.tsx
@@ -2,9 +2,13 @@ import type { FormEvent } from 'react'
 import { FcGoogle } from 'react-icons/fc'
 import { FaGithub } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6'
+import { supabase } from '../../libs/supabaseClient'
 export const SignInForm = () => {
   const submitHandler = async (e: FormEvent) => {
     e.preventDefault()
+  }
+  const loginWithGithub = async () => {
+    supabase.auth.signInWithOAuth({ provider: 'github' })
   }
   return (
     <form
@@ -43,41 +47,28 @@ export const SignInForm = () => {
           required
         />
       </label>
-      <label className='input input-bordered flex items-center gap-2'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 16 16'
-          fill='currentColor'
-          className='w-4 h-4 opacity-70'
-        >
-          <path
-            fillRule='evenodd'
-            d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
-            clipRule='evenodd'
-          ></path>
-        </svg>
-        <input
-          type='password'
-          className='grow'
-          placeholder='Confirm Password'
-          required
-        />
-      </label>
+
       <button className='btn btn-primary' type='submit'>
         Sign Up
       </button>
 
       <div className='divider'>OR</div>
 
-      <button className='btn btn-outline' type='button'>
+      <button className='btn btn-outline' type='button' disabled>
         <FcGoogle size={30} />
         Login with Google
       </button>
-      <button className='btn btn-outline' type='button'>
+      <button
+        className='btn btn-outline'
+        type='button'
+        onClick={() => {
+          loginWithGithub()
+        }}
+      >
         <FaGithub size={30} />
         Login with GitHub
       </button>
-      <button className='btn btn-outline' type='button'>
+      <button className='btn btn-outline' type='button' disabled>
         <FaXTwitter size={30} />
         Login with X
       </button>
--- src/component/AuthMenu.tsx
+++ src/component/AuthMenu.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { supabase } from '../libs/supabaseClient'
 import type { Session } from '@supabase/supabase-js'
+import Avatar from 'boring-avatars'
 
 export const AuthMenu = () => {
   const [session, setSession] = useState<null | Session>(null)
@@ -8,7 +9,7 @@ export const AuthMenu = () => {
     supabase.auth.getSession().then((session) => {
       setSession(session.data.session)
     })
-  })
+  }, [])
 
   const signUp = async () => {
     window.location.href = '/auth/sign_up'
@@ -16,9 +17,30 @@ export const AuthMenu = () => {
   const signIn = async () => {
     window.location.href = '/auth/sign_in'
   }
+  const signOut = async () => {
+    await supabase.auth.signOut()
+    setSession(null)
+  }
 
   if (session) {
-    return <div>ログイン済</div>
+    return (
+      <div>
+        <div className='avatar flex items-center gap-2'>
+          <div className='w-9 rounded-full'>
+            <Avatar
+              size={40}
+              name={session.user.user_metadata.name}
+              variant='marble'
+              colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
+            />
+          </div>
+          {session.user.user_metadata.name}
+          <button className='btn' onClick={signOut}>
+            Sign Out
+          </button>
+        </div>
+      </div>
+    )
   } else {
     return (
       <div className='flex gap-2'>
