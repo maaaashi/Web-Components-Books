6507ca6a4a57ae9bc7b328ade5cfdd3d10c88a63
maaaashi <doyaseizin.7@gmail.com>
2024-04-01 19:25:28 +0900

    auth追加

--- src/component/Auth/SignIn.tsx
+++ src/component/Auth/SignIn.tsx
@@ -1,11 +1,18 @@
-import type { FormEvent } from 'react'
+import { useState, type FormEvent } from 'react'
 import { FcGoogle } from 'react-icons/fc'
 import { FaGithub } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6'
 import { supabase } from '../../libs/supabaseClient'
 export const SignInForm = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const submitHandler = async (e: FormEvent) => {
     e.preventDefault()
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
   }
   return (
     <form
@@ -20,9 +27,18 @@ export const SignInForm = () => {
           fill='currentColor'
           className='w-4 h-4 opacity-70'
         >
-          <path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z'></path>
+          <path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
+          <path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
         </svg>
-        <input type='text' className='grow' placeholder='Username' required />
+        <input
+          type='text'
+          className='grow'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+        />
       </label>
       <label className='input input-bordered flex items-center gap-2'>
         <svg
@@ -42,6 +58,10 @@ export const SignInForm = () => {
           className='grow'
           placeholder='Password'
           required
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
         />
       </label>
 
--- src/component/Auth/SignUp.tsx
+++ src/component/Auth/SignUp.tsx
@@ -1,12 +1,29 @@
-import type { FormEvent } from 'react'
+import { useState, type FormEvent } from 'react'
 import { supabase } from '../../libs/supabaseClient'
 import { FcGoogle } from 'react-icons/fc'
 import { FaGithub } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6'
 
 export const SignUpForm = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   const submitHandler = async (e: FormEvent) => {
     e.preventDefault()
+
+    if (password !== confirmPassword) {
+      alert('パスワードが一致しません')
+      return
+    }
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    })
+
+    if (error) {
+      console.log(error)
+    }
   }
   return (
     <form
@@ -21,9 +38,18 @@ export const SignUpForm = () => {
           fill='currentColor'
           className='w-4 h-4 opacity-70'
         >
-          <path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z'></path>
+          <path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
+          <path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
         </svg>
-        <input type='text' className='grow' placeholder='Username' required />
+        <input
+          type='text'
+          className='grow'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+        />
       </label>
       <label className='input input-bordered flex items-center gap-2'>
         <svg
@@ -43,6 +69,10 @@ export const SignUpForm = () => {
           className='grow'
           placeholder='Password'
           required
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
         />
       </label>
       <label className='input input-bordered flex items-center gap-2'>
@@ -63,6 +93,10 @@ export const SignUpForm = () => {
           className='grow'
           placeholder='Confirm Password'
           required
+          value={confirmPassword}
+          onChange={(e) => {
+            setConfirmPassword(e.target.value)
+          }}
         />
       </label>
       <button className='btn btn-primary' type='submit'>
