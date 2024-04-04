9f90e68467f8b0f77a04ae56ac1690c0cb3b8ea3
maaaashi <doyaseizin.7@gmail.com>
2024-04-05 07:42:25 +0900

    登録ボタン表示

--- src/component/SideMenuItems.tsx
+++ src/component/SideMenuItems.tsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from 'react'
 import { SideMenuItem } from './Button/SideMenuItem'
 import { supabase } from '../libs/supabaseClient'
+import type { Session } from '@supabase/supabase-js'
 
 export const SideMenuItems = () => {
   const [items, setItems] = useState<{ title: string; id: string }[]>()
+  const [session, setSession] = useState<Session | null>()
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      setSession(data.session)
+    })
+  }, [session])
 
   useEffect(() => {
     supabase
@@ -29,6 +37,8 @@ export const SideMenuItems = () => {
 
   return (
     <ul className='flex flex-col gap-3'>
+      {session && <button className='btn btn-primary w-full'>登録</button>}
+
       {items.map((l) => {
         return (
           <li key={l.id}>
