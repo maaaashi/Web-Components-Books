8fbf6f5ce4e01c425dd43cd631eb657bcb8a2e16
maaaashi <doyaseizin.7@gmail.com>
2024-04-10 07:51:54 +0900

    insert

--- src/component/Button/RegisterWebcomponent.tsx
+++ src/component/Button/RegisterWebcomponent.tsx
@@ -2,8 +2,19 @@ import type { Session } from '@supabase/supabase-js'
 import { useEffect, useRef, useState, type FormEvent } from 'react'
 import { supabase } from '../../libs/supabaseClient'
 
+type InsertWebcomponent = {
+  name: string
+  description: string
+  tagname: string
+  src: string
+}
+
 export const RegisterWebcomponentButton = () => {
   const [session, setSession] = useState<Session | null>()
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [tagname, setTagname] = useState('')
+  const [src, setSrc] = useState('')
   const modalRef = useRef<HTMLDialogElement>(null)
 
   const openModal = () => {
@@ -11,8 +22,25 @@ export const RegisterWebcomponentButton = () => {
     modalRef.current.showModal()
   }
 
+  const insertWebcomponent = async () => {
+    const { data, error } = await supabase
+      .from('webcomponent')
+      .insert<InsertWebcomponent>({
+        name,
+        description,
+        tagname,
+        src,
+      })
+    if (error) {
+      console.error(error)
+    } else {
+      console.log(data)
+    }
+  }
+
   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    insertWebcomponent()
   }
 
   useEffect(() => {
@@ -36,9 +64,11 @@ export const RegisterWebcomponentButton = () => {
                 <span className='label-text'>Name</span>
               </div>
               <input
-                type='text'
-                placeholder='例) Country Flag Component'
                 className='input input-bordered w-full'
+                placeholder='例) Country Flag Component'
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </label>
             <label className='form-control'>
@@ -48,6 +78,8 @@ export const RegisterWebcomponentButton = () => {
               <textarea
                 className='textarea textarea-bordered h-24'
                 placeholder='例) 国旗を表示するコンポーネント'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               ></textarea>
             </label>
             <label className='form-control w-full'>
@@ -58,6 +90,8 @@ export const RegisterWebcomponentButton = () => {
                 type='text'
                 placeholder='例) country-flag'
                 className='input input-bordered w-full'
+                value={tagname}
+                onChange={(e) => setTagname(e.target.value)}
               />
             </label>
             <label className='form-control w-full'>
@@ -68,6 +102,8 @@ export const RegisterWebcomponentButton = () => {
                 type='text'
                 placeholder='例) https://maaaashi.github.io/country-flag/bundle.js'
                 className='input input-bordered w-full'
+                value={src}
+                onChange={(e) => setSrc(e.target.value)}
               />
             </label>
             <button type='submit' className='btn btn-primary mt-6'>
